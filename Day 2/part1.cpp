8ee5b45cbcfa67805283598eb12db7b929ecfeaf
oaaees <jmpareds@gmail.com>
2022-12-22 03:03:58 -0400

    added day 2 part 1

---
+++ Day 2/part1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std; 
+
+int main(){
+    string line;
+    int sum = 0;
+
+    // Read File
+    ifstream ReadFile("input.txt");
+
+    while (getline(ReadFile, line)) {
+        int ply1 = line[0] - 64;
+        int ply2 = line[2] - 87;
+        
+        sum += ply2;
+
+        if(ply2 == ply1+1 || (ply2 == 1 && ply1 == 3)){
+            sum += 6;
+        }
+
+        if(ply1 == ply2){
+            sum += 3;
+        }
+    }
+
+    ReadFile.close(); 
+
+    cout << "Score is: " << sum << endl;
+
+    return 0;
+}
