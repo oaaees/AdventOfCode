21a68f6fd30444b42f2dcd6a35e2fcd4f2f67abe
Johan Paredes <jmpareds@gmail.com>
2023-12-03 13:38:28 -0400

    added Day 2 part 2 2023

---
+++ 2023/02p2.cpp
@@ -0,0 +1,120 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Game {
+    string full;
+    vector<string> sets;
+    vector<vector<pair<int, char>>> plays;
+    int ID;
+
+    Game(string line) {
+        full = line;
+        setID();
+        setSets();
+        setPlays();
+    }
+
+    void setID(){
+        string s = "";
+        for(int i = 0; i < full.size(); i++){
+            if(full[i] >= 48 && full[i] <= 57) s += full[i];
+            if(full[i] == ':'){
+                full = full.substr(i+2);
+                ID = stoi(s);
+                break;
+            } 
+        }
+    }
+
+    void setSets(){
+        int last = 0;
+        for (int i = 0; i < full.length(); i++){
+            if(full[i] == ';'){
+                sets.push_back(full.substr(last, i - last));
+                i += 2;
+                last = i;
+            }
+        }
+        sets.push_back(full.substr(last));
+    }
+
+    void setPlays(){
+        vector<pair<int, char>> v = {};
+        vector<vector<pair<int, char>>> mv = {};
+
+        for(int i = 0; i < sets.size(); i++){
+            string s = "";
+            for(int j = 0; j < sets[i].size(); j++){
+                if(sets[i][j] == ' '){
+                    int n = stoi(s);
+                    char c = sets[i][j + 1];
+                    v.push_back(make_pair(n,c));
+                    s = "";
+
+                    switch(c){
+                        case 'r':
+                        j += 5;
+                        break;
+                        case 'b':
+                        j += 6;
+                        break;
+                        case 'g':
+                        j += 7;
+                        break;
+                    }
+                } else {
+                    s += sets[i][j];
+                }
+            }
+            mv.push_back(v);
+            v = {};
+        }
+
+        plays = mv;
+    }
+
+    ~Game(){}
+};
+
+int main (){
+    ifstream file;
+    file.open("input.txt");
+
+    string line = "";
+    int sum = 0;
+
+    while( file.good() ){
+        getline(file, line);
+        
+        Game game = Game(line);
+        int blue = 0, red = 0, green = 0;
+
+        for(int i = 0; i < game.plays.size(); i++){
+            for(int j = 0; j < game.plays[i].size(); j++){
+                switch(game.plays[i][j].second){
+                    case 'r':
+                    if (red < game.plays[i][j].first) red = game.plays[i][j].first;
+                    break;
+                    case 'b':
+                    if (blue < game.plays[i][j].first) blue = game.plays[i][j].first;
+                    break;
+                    case 'g':
+                    if (green < game.plays[i][j].first) green = game.plays[i][j].first;
+                    break;
+                    default:
+                    break;
+                }
+            }
+        }
+
+        sum += (red * blue * green);
+    }
+
+    file.close();
+    cout << sum;
+
+    return 0;
+}
