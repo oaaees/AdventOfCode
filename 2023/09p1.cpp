023dadac3ff6b44819630f72fc101e57cea551db
Johan Paredes <jmpareds@gmail.com>
2023-12-09 20:58:20 -0400

    added day 9 part 1 2023

---
+++ 2023/09p1.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+vector<string> split(string &s, char c){
+    vector<string> v;
+    int last = 0;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == c){ 
+            v.push_back(s.substr(last, i - last));
+            last = i + 1;
+        }
+    }
+    v.push_back(s.substr(last));
+    return v;
+};
+
+vector<int> get_nums(string s){
+    vector<string> temp = split(s, ' ');
+    vector<int> res = {};
+
+    for(int i = 0; i < temp.size(); i++){
+        res.push_back(stoi(temp[i]));
+    }
+
+    return res;
+};
+
+bool is_all_zeroes(vector<int> v){
+    for(int i = 0; i < v.size(); i++) if (v[i] != 0) return false;
+    return true;
+}
+
+int main (){
+    ifstream file;
+    file.open("input.txt");
+
+    string s = "";
+    vector<vector<int>> nums;
+    int sum = 0;
+
+    while( file.good() ){
+        getline(file,s);
+        nums = {};
+        nums.push_back(get_nums(s));
+
+        while(!is_all_zeroes(nums.back())){
+            nums.push_back(vector<int>(nums.back().size() - 1, 0));
+            for(int i = 0; i < nums.back().size(); i++){
+                nums[nums.size() - 1][i] = nums[nums.size() - 2][i+1] - nums[nums.size() - 2][i];
+            }
+        }
+
+        nums.back().push_back(0);
+
+        for(int i = nums.size() - 2; i >= 0; i--){
+            nums[i].push_back(nums[i].back() + nums[i+1].back());
+        }
+
+        sum += nums[0].back();
+    }
+
+    file.close();
+    cout << sum;
+
+    return 0;
+}
